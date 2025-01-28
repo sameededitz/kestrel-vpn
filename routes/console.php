7c91df1d42be7495b489c942fec4967913225368
Sameed <sameedhassan22@gmail.com>
2025-01-28 18:48:57 +0500

    Add command to remove expired purchases and update schedule frequency

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('auth:clear-resets')->everyFifteenMinutes();
+Schedule::command('auth:clear-resets')->everySixHours();
+
+Schedule::command('purchases:remove-expired')->daily();
